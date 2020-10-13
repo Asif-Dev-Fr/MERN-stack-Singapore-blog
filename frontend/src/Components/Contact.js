7bd44a5954218577c460450b5caf48b77051c797
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-10-13 10:55:35 +0200

    improvement contact

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -2,40 +2,40 @@ import React from 'react'
 
 const Contact = () => {
     return(
-        <div class="center contact">
+        <div className="center contact">
 
-    		<h1 class="title-center"> Contactez-nous si vous avez une question ou une remarque</h1>
+    		<h1 className="title-center"> Contactez-nous si vous avez une question ou une remarque</h1>
  
 			<p><i> Tous les champs sont obligatoires </i></p>
-			<div class="row">
-				<div class="form-group col-12 col-sm-12 col-md-12 col-lg-4">
+			<div className="row">
+				<div className="form-group col-12 col-sm-12 col-md-12 col-lg-4">
 					<input type="text" name="nom" placeholder="Nom" className="form-control"  />
 				</div>
-				<div class="form-group col-12 col-sm-12 col-md-12 col-lg-4">
+				<div className="form-group col-12 col-sm-12 col-md-12 col-lg-4">
 					<input type="text" name="prenom" placeholder="Prénom" className="form-control"  />
 				</div>
-				<div class="form-group col-12 col-sm-12 col-md-12 col-lg-4">
+				<div className="form-group col-12 col-sm-12 col-md-12 col-lg-4">
 					
 					<input type="text" placeholder='Email'name="email" className="form-control"  />
 					
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="form-group col-12 col-sm-12 col-md-12 col-lg-12">
+			<div className="row">
+				<div className="form-group col-12 col-sm-12 col-md-12 col-lg-12">
                     <input type="text" name="objec
                     t" placeholder="Objet de votre message" className="form-control"  />
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="form-group col-12 col-sm-12 col-md-12 col-lg-12 messageContact">
+			<div className="row">
+				<div className="form-group col-12 col-sm-12 col-md-12 col-lg-12 messageContact">
 					<textarea className="form-control messageContact" placeholder="Votre message"></textarea>
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="form-group contactSubmitButton">
+			<div className="row">
+				<div className="form-group contactSubmitButton">
 					<button className="btn buttonRed">Envoyer</button>
 				</div>
 			</div>
